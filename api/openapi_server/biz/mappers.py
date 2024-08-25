08e1798cc2e56b0d6f60f39d1b810165926ce7a0
Asa Sprow <asasprow@gmail.com>
2024-08-25 15:16:43 -0600

    make max and min sessions nullable and default to null and use param names in constructors in mappers

--- api/openapi_server/biz/mappers.py
+++ api/openapi_server/biz/mappers.py
@@ -13,13 +13,13 @@ def map_db_tasks_to_tasks(db_tasks: list[models.Task]) -> list[Task]:
 
 def map_db_task_to_task(db_task: models.Task) -> Task:
     return Task(
-        db_task.id,
-        db_task.description,
-        db_task.points,
-        db_task.min_session,
-        db_task.max_session,
-        db_task.enabled,
-        map_db_categories_to_categories(db_task.categories)
+        id = db_task.id,
+        description = db_task.description,
+        points = db_task.points,
+        min_session = db_task.min_session,
+        max_session = db_task.max_session,
+        enabled = db_task.enabled,
+        categories = map_db_categories_to_categories(db_task.categories)
     )
 
 def map_db_categories_to_categories(db_categories: list[models.Category]) -> list[Category]:
@@ -29,18 +29,18 @@ def map_db_categories_to_categories(db_categories: list[models.Category]) -> lis
 
 def map_db_category_to_category(db_category: models.Category) -> Category:
     return Category(
-        db_category.id,
-        db_category.name,
-        db_category.description
+        id = db_category.id,
+        name = db_category.name,
+        description = db_category.description
     )
 
 def map_db_player_to_player(db_player: models.Player) -> Player:
     return Player(
-        db_player.id,
-        db_player.discord_id,
-        db_player.score,
-        map_db_categories_to_categories(db_player.categories),
-        map_db_tasks_to_tasks(db_player.tasks)
+        id = db_player.id,
+        discord_id = db_player.discord_id,
+        score = db_player.score,
+        categories = map_db_categories_to_categories(db_player.categories),
+        tasks = map_db_tasks_to_tasks(db_player.tasks)
     )
 
 def map_db_players_to_player_lites(db_players: list[models.Player]) -> list[PlayerLite]:
@@ -50,8 +50,8 @@ def map_db_players_to_player_lites(db_players: list[models.Player]) -> list[Play
 
 def map_db_player_to_player_lite(db_player: models.Player) -> PlayerLite:
     return PlayerLite(
-        db_player.id,
-        db_player.discord_id,
-        db_player.score,
-        map_db_categories_to_categories(db_player.categories)
+        id = db_player.id,
+        discord_id = db_player.discord_id,
+        score = db_player.score,
+        categories = map_db_categories_to_categories(db_player.categories)
     )
